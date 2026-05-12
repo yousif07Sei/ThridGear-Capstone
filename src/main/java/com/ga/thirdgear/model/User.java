fd97b6059a5098c6776de9ce5d25469debaae4aa
yousif <yousif.sei07@gmail.com>
2026-05-12 14:40:50 +0300

    add the User Model

---
+++ src/main/java/com/ga/thirdgear/model/User.java
@@ -0,0 +1,77 @@
+package com.ga.thirdgear.model;
+
+import com.ga.thirdgear.enums.UserRole;
+import com.ga.thirdgear.enums.UserStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "First name is required")
+    @Column(nullable = false)
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Column(nullable = false)
+    private String lastName;
+
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "Email is required")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Column(nullable = false)
+    private  String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status;
+
+    @Column
+    private String profilePicture;
+
+    @Column(nullable = false)
+    private boolean emailVerified = false;
+
+    private String resetToken;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column
+    private LocalDateTime deletedAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Car> cars;
+
+
+    @OneToMany(mappedBy = "buyer", cascade = CascadeType.ALL)
+    private List<Inquiry> inquiries;
+}
