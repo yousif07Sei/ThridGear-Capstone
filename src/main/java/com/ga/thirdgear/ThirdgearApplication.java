5c64edb82d0b36f23f0899984c32e734819fbb29
yousif <yousif.sei07@gmail.com>
2026-05-12 14:24:25 +0300

    project setup added JWT dependcy  , foler structure

---
+++ src/main/java/com/ga/thirdgear/ThirdgearApplication.java
@@ -0,0 +1,13 @@
+package com.ga.thirdgear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdgearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdgearApplication.class, args);
+	}
+
+}
