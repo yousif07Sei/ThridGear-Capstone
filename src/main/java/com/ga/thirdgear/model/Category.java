d7af3e804befe126a6c5ca605f36c941abcc8291
yousif <yousif.sei07@gmail.com>
2026-05-12 19:26:32 +0300

    add the Category Model

---
+++ src/main/java/com/ga/thirdgear/model/Category.java
@@ -0,0 +1,40 @@
+package com.ga.thirdgear.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = " Category  name is required")
+    @Column(nullable = false)
+    private String Name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Car> cars;
+
+}
