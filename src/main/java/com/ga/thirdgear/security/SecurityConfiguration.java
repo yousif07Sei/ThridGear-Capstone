e0334749d0e4ab3b328505e4ddb563be4dc4c17d
Yousif <yousif.sei07@gmail.com>
2026-05-15 20:50:11 +0300

    add the JwtUtils and move the security config to the right place

--- src/main/java/com/ga/thirdgear/security/SecurityConfiguration.java
+++ src/main/java/com/ga/thirdgear/security/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.ga.thirdgear.service;
+package com.ga.thirdgear.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
