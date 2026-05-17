aeaf155821d27bf7e1915409d3b6fed682c240d0
yousif <yousif.sei07@gmail.com>
2026-05-17 16:28:43 +0300

    add InformationExistException

---
+++ src/main/java/com/ga/thirdgear/exception/InformationExistException.java
@@ -0,0 +1,7 @@
+package com.ga.thirdgear.exception;
+
+public class InformationExistException extends RuntimeException {
+    public InformationExistException(String message) {
+        super(message);
+    }
+}
