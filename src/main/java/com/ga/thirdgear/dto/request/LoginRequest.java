6064ffb956b3e4c7a6a6fe541e1ade1bf64f51d1
yousif <yousif.sei07@gmail.com>
2026-05-17 10:49:54 +0300

    add Login request

---
+++ src/main/java/com/ga/thirdgear/dto/request/LoginRequest.java
@@ -0,0 +1,19 @@
+package com.ga.thirdgear.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+
+@Data
+public class LoginRequest {
+
+
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
