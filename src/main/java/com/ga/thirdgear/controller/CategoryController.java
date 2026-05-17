7daf3853da9797d8eb949539f7f778517eb6df3e
yousif <yousif.sei07@gmail.com>
2026-05-18 02:22:38 +0300

    add CategoryController and fix the field name in the model

---
+++ src/main/java/com/ga/thirdgear/controller/CategoryController.java
@@ -0,0 +1,48 @@
+package com.ga.thirdgear.controller;
+
+import com.ga.thirdgear.model.Category;
+import com.ga.thirdgear.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private CategoryService categoryService;
+
+    @Autowired
+    public void setCategoryService(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+        return ResponseEntity.ok(categoryService.getCategoryById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.createCategory(category));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        return ResponseEntity.ok(categoryService.updateCategory(id, category));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/ga/thirdgear/model/Category.java
+++ src/main/java/com/ga/thirdgear/model/Category.java
@@ -24,8 +24,8 @@ public class Category {
     private Long id;
 
     @NotBlank(message = " Category  name is required")
-    @Column(nullable = false)
-    private String Name;
+    @Column(nullable = false, unique = true)
+    private String name;
 
     @Column(columnDefinition = "TEXT")
     private String description;
