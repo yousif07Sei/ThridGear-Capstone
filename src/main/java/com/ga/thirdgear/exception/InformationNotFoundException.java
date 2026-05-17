bcdd4b0fc0fc4e7c5338629700aae16c3f4f5b1c
yousif <yousif.sei07@gmail.com>
2026-05-17 16:29:49 +0300

    add InformationNotFoundException

---
+++ src/main/java/com/ga/thirdgear/exception/InformationNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ga.thirdgear.exception;
+
+public class InformationNotFoundException extends RuntimeException {
+    public InformationNotFoundException(String message) {
+        super(message);
+    }
+}
